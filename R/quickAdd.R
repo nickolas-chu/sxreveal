5fadb6bce537b1ea556e2a65c23543a2631bd819
Nickolas Chu <91218484+nickolas-chu@users.noreply.github.com>
2025-11-27 11:22:28 -0800

    Update quickAdd.R

--- R/quickAdd.R
+++ R/quickAdd.R
@@ -8,20 +8,24 @@
 #' @param Seuratobj A target Seurat object
 #' @export
 #' @author Nickolas C. Chu
-quickAdd <- function(Probabilities, Seuratobj )
-{
-  Seuratobj[["Ygenes2"]] <- Probabilities$Ygenes
-  Seuratobj[["Xgenes2"]] <- Probabilities$Xgenes
-  Seuratobj[["Xist2"]] <- Probabilities$Xist
-  Seuratobj[["ProbUni"]] <- Probabilities$ProbFemaleUni
-  Seuratobj[["SexUni"]] <- Probabilities$SexUni
-  Seuratobj[["ProbMulti"]] <- Probabilities$ProbFemaleMulti
-  Seuratobj[["SexMulti"]] <- Probabilities$SexMulti
-  Seuratobj[["ProbMultinCount"]] <- Probabilities$ProbFemaleMultinCount
-  Seuratobj[["SexMultinCount"]] <- Probabilities$SexMultinCount
-  Seuratobj[["ProbMultiXY"]] <- Probabilities$ProbFemaleXY
-  Seuratobj[["SexMultiXY"]] <- Probabilities$SexMultiXY
-
+quickAdd <- function(Probabilities, Seuratobj ){
+  # Ensure cell_id column exists
+  if (!"cell_id" %in% colnames(Probabilities)) {
+    stop("Probabilities dataframe must contain a 'cell_id' column")
+  }
+  cols_to_add <- setdiff(colnames(Probabilities), c("cell_id", "cluster"))
+  
+  # Align by cell_id without reordering Seurat metadata
+  meta <- Seuratobj@meta.data
+  # Match rows by cell_id
+  idx <- match(rownames(meta), Probabilities$cell_id)
+  # For each column, add values into metadata
+  for (col in cols_to_add) {
+    meta[[col]] <- Probabilities[[col]][idx]
+  }
+  # Replace metadata
+  Seuratobj@meta.data <- meta
+  
   return(Seuratobj)
 
 }
